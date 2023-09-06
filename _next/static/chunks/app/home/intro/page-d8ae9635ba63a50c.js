4e7f991cafb3e83cecd50408ecc892c5abd8c9dd
Oscar Javier Herrera Rodriguez <82051248+oshero0295@users.noreply.github.com>
2023-09-06 15:07:59 -0600

    initial commit

---
+++ _next/static/chunks/app/home/intro/page-d8ae9635ba63a50c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[63],{2:function(n,e,u){Promise.resolve().then(u.t.bind(u,8130,23))},8130:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=2)}),_N_E=n.O()}]);
